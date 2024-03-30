93d816dcffa41740ade0eaa09568e81d7d9bf76a
MyNextWeekend <jinhu007@outlook.com>
2024-03-30 10:54:22 +0800

    枚举

--- src/main/java/com/example/studyspringboot/constant/ModelTypeConstant.java
+++ src/main/java/com/example/studyspringboot/constant/ModelTypeConstant.java
@@ -7,7 +7,38 @@ package com.example.studyspringboot.constant;
  * @date 2024/01/27
  */
 public class ModelTypeConstant {
-    public static final Integer CNC = 0;
-    public static final Integer AI = 1;
-    public static final Integer CLOUD = 2;
+    public enum StateEnum {
+        INIT(0, "初始化"),
+        RUNNING(1, "执行中"),
+        SUCCESS(2, "成功"),
+        FAIL(5, "失败"),
+        WAITING(6, "等待"),
+        ;
+        private final int code;
+        private final String desc;
+
+        StateEnum(int code, String desc) {
+            this.code = code;
+            this.desc = desc;
+        }
+
+        public int getCode() {
+            return code;
+        }
+
+        public String getDesc() {
+            return desc;
+        }
+        public static StateEnum getByCode(Integer code) {
+            if (code == null) {
+                return null;
+            }
+            for (StateEnum stateEnum : StateEnum.values()) {
+                if (stateEnum.getCode() == code) {
+                    return stateEnum;
+                }
+            }
+            return null;
+        }
+    }
 }
--- src/main/java/com/example/studyspringboot/controller/DemoController.java
+++ src/main/java/com/example/studyspringboot/controller/DemoController.java
@@ -1,11 +1,13 @@
 package com.example.studyspringboot.controller;
 
 import com.example.studyspringboot.common.Result;
+import com.example.studyspringboot.constant.ModelTypeConstant;
 import com.example.studyspringboot.constant.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,7 +19,26 @@ public class DemoController {
     private final Logger logger = LoggerFactory.getLogger(DemoController.class);
 
     @GetMapping("/hello")
-    public Result<?> hello() {
+    public Result<?> hello( @RequestParam Integer code) {
+        ModelTypeConstant.StateEnum byCode = ModelTypeConstant.StateEnum.getByCode(code);
+        if(byCode==null){
+            logger.info("error");
+            return Result.error(1,"error");
+        }
+        switch (byCode){
+            case SUCCESS:
+                logger.info("SUCCESS");
+                break;
+            case FAIL:
+                logger.info("FAIL");
+                break;
+            case WAITING:
+                logger.info("WAITING");
+                break;
+            default:
+                logger.info("default");
+        }
+
         logger.info("/hello接收到请求");
         logger.info("do work 001 success");
         logger.info("do work 002 success");
